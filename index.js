5e7e42e11fdfb5eba91ddb0cb17dd327139e00d4
jesuszavalagt <jesus.zavala@gametime.co>
2022-11-24 18:03:47 -0800

    test endpoint for uploading image

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const multer = require("multer");
+const path = require("path");
 
 dotenv.config();
 const app = express();
@@ -24,9 +26,48 @@ const partidosRoutes = require("./routes/partidos");
 const municipiosRoutes = require("./routes/municipios");
 const verifyToken = require("./routes/validate-token");
 
-app.use(express.json()); // for body parser
-app.use("/api/user", authRoutes);
+const checkFileType = function (file, cb) {
+  //Allowed file extensions
+  const fileTypes = /jpeg|jpg|png|gif|svg/;
 
+  //check extension names
+  const extName = fileTypes.test(path.extname(file.originalname).toLowerCase());
+
+  const mimeType = fileTypes.test(file.mimetype);
+
+  if (mimeType && extName) {
+    return cb(null, true);
+  } else {
+    cb("Error: You can Only Upload Images!!");
+  }
+};
+
+//Setting storage engine
+const storageEngine = multer.diskStorage({
+  destination: "./images",
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}--${file.originalname}`);
+  },
+});
+//initializing multer
+const upload = multer({
+  storage: storageEngine,
+  limits: { fileSize: 1000000 },
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  },
+});
+
+app.use(express.json()); // for body parser
+app.post("/single", upload.single("image"), (req, res) => {
+  if (req.file) {
+    res.send("Single file uploaded successfully");
+  } else {
+    res.status(400).send("Please upload a valid image");
+  }
+});
+
+app.use("/api/user", authRoutes);
 // this route is protected with token
 app.use("/api/dashboard", verifyToken, dashboardRoutes);
 app.use("/api/partidos", verifyToken, partidosRoutes);
